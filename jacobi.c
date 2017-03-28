736762299b2f76317a75a3d60438f74d6163ba6d
Li Zhaoyang <lizy14@yeah.net>
2017-03-28 19:51:43 +0800

    edge cases

--- jacobi.c
+++ jacobi.c
@@ -51,17 +51,17 @@ int*** jacobi(int*** original, int d, int h, int w, int step) {
 	int*** other_grid = original;
 
 	for (int t = 0; t < step; t++) {		
-		for (int z = 0; z < d; z++) {
-			for (int y = 0; y < h; y++) {
-				for (int x = 0; x < w; x++) {
+		for (int z = 1; z < d - 1; z++) {
+			for (int y = 1; y < h - 1; y++) {
+				for (int x = 1; x < w - 1; x++) {
 					// grid[x][y][z] = avg of neighbors in other_grid*/
 					int sum_neibours =
-						other_grid[z][y][min(x + 1, w-1)] +
-						other_grid[z][y][max(x - 1, 0)] +
-						other_grid[z][min(y + 1, h-1)][x] +
-						other_grid[z][max(y - 1, 0)][x] +
-						other_grid[min(z + 1, d-1)][y][x] +
-						other_grid[max(z - 1, 0)][y][x];
+						other_grid[z][y][x + 1] +
+						other_grid[z][y][x - 1] +
+						other_grid[z][y + 1][x] +
+						other_grid[z][y - 1][x] +
+						other_grid[z + 1][y][x] +
+						other_grid[z - 1][y][x];
 
 					grid[z][y][x] = sum_neibours / 6;
 				}
