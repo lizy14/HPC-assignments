022dff9fc18ef6c5721eb654126d3b8f1ac9db30
Zhaoyang <lizy14@yeah.net>
2017-05-29 22:38:08 -0400

    it works\!

--- jacobi-mpi/main.c
+++ jacobi-mpi/main.c
@@ -7,11 +7,6 @@
 extern int Init(double *data, long long L);
 extern int Check(double *data, long long L);
 
-//Can be modified
-typedef struct
-{
-    int nx, ny, nz;
-} Info;
 
 int test(){
     return test_partition();
@@ -50,37 +45,26 @@ int index_of_last_element_in_group(int i, int n, int k){
     return result;
 }
 
-//Must be modified because only single process is used now
-Info setup(int NX, int NY, int NZ, int P)
-{
-    Info result;
-    int myrank = 0;
-    MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
-    if(myrank == 0)
-    {
-        result.nx = NX;
-        result.ny = NY;
-        result.nz = NZ;
-    }
-    else
-    {
-        result.nx = 0;
-        result.ny = 0;
-        result.nz = 0;
-    }
-    return result;
-}
 
 
 //Must be re-written, including all the parameters
-int stencil(double *A, double *B, int nx, int ny, int nz, int steps)
+int stencil(
+        double *A, double *B,
+        int nx, int ny, int nz, int steps,
+        int first, int last, int rank)
 {
     int i, j, k, s;
-#define IDX(i,j,k) ((i)*ny*nz+(j)*nz+(k))
+
+#define IDX(i,j,k) ((i) + (j)*nx + (k)*nx*ny)
+
     for(s = 0; s < steps; s ++) {
-        for(i = 0; i < nx; i ++) {
+
+        for(k = first; k <= last; k ++) {
+
             for(j = 0; j < ny; j ++) {
-                for(k = 0; k < nz; k ++) {
+                for(i = 0; i < nx; i ++) {
+
+
                     double r = 0.4*A[IDX(i,j,k)];
                     if(k !=  0)   r += 0.1*A[IDX(i,j,k-1)];
                     else          r += 0.1*A[IDX(i,j,k)];
@@ -98,6 +82,55 @@ int stencil(double *A, double *B, int nx, int ny, int nz, int steps)
                 }
             }
         }
+        // message passing
+        int s = nx * ny;
+        MPI_Request reqs[4];
+        MPI_Status stats[4];
+        int i_am_head = first == 0;
+        int i_am_tail = last == nz - 1;
+        if(i_am_tail && i_am_head){
+            // only one worker, nothing done
+        }else{
+            if(! i_am_head){
+                MPI_Isend(B + IDX(0,0,first),
+                        s, MPI_DOUBLE,
+                        rank - 1,
+                        0, MPI_COMM_WORLD,
+                        &reqs[0]
+                        );
+                MPI_Irecv(B + IDX(0,0,first - 1),
+                        s, MPI_DOUBLE,
+                        rank - 1,
+                        0, MPI_COMM_WORLD,
+                        &reqs[1]
+                        );
+            }
+            if(! i_am_tail){
+                MPI_Isend(B + IDX(0,0,last),
+                        s, MPI_DOUBLE,
+                        rank + 1,
+                        0, MPI_COMM_WORLD,
+                        &reqs[2]
+                        );
+                MPI_Irecv(B + IDX(0,0,last + 1),
+                        s, MPI_DOUBLE,
+                        rank + 1,
+                        0, MPI_COMM_WORLD,
+                        &reqs[3]
+                        );
+            }
+            if(! i_am_tail && ! i_am_head){
+                MPI_Waitall(4, reqs, stats);
+            }else if(i_am_tail){
+                MPI_Waitall(2, reqs, stats);
+            }else if(i_am_head){
+                MPI_Waitall(2, reqs+2, stats);
+            }
+        }
+
+
+
+
         double *tmp = NULL;
         tmp = A, A = B, B = tmp;
     }
@@ -105,7 +138,7 @@ int stencil(double *A, double *B, int nx, int ny, int nz, int steps)
 }
 
 int main(int argc, char **argv) {
-    return test();
+//    return test();
 
     double *A = NULL, *B = NULL;
     int myrank, nprocs, nx, ny, nz;
@@ -115,30 +148,41 @@ int main(int argc, char **argv) {
     MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
     MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
 
-    NX = atoi(argv[1]);
-    NY = atoi(argv[2]);
-    NZ = atoi(argv[3]);
-    STEPS = atoi(argv[4]);
+    if(argc >= 5){
+        NX = atoi(argv[1]);
+        NY = atoi(argv[2]);
+        NZ = atoi(argv[3]);
+        STEPS = atoi(argv[4]);
+    }
 
     if(myrank == 0){
         printf("Size: %dx%dx%d, # of Steps: %d, # of procs: %d\n",
                 NX, NY, NZ, STEPS, nprocs);
     }
-    Info info = setup(NX, NY, NZ, nprocs); //This is a single version
-    nx = info.nx, ny = info.ny, nz = info.nz;
 
-    long long size = nx*ny*nz;
+    nx = NX, ny = NY, nz = NZ;
+
+    // allocate matricies
+    long long size = nx * ny * nz;
 
     A = (double*)malloc(size*sizeof(double));
     B = (double*)malloc(size*sizeof(double));
 
+    // initialize matrix
     Init(A, size);
+
+
+    // get assignment
+    int n = number_of_elements_in_group(myrank, nz, nprocs);
+    int last = index_of_last_element_in_group(myrank, nz, nprocs);
+    int first = last - n + 1;
+
 
     struct timeval t1, t2;
     MPI_Barrier(MPI_COMM_WORLD);
     gettimeofday(&t1, NULL);
 
-    stencil(A, B, nx, ny, nz, STEPS);
+    stencil(A, B, nx, ny, nz, STEPS, first, last, myrank);
 
     MPI_Barrier(MPI_COMM_WORLD);
     gettimeofday(&t2, NULL);
